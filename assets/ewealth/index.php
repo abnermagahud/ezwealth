abc0addb6cba0b753aa0d88f796703be13ad92f0
Abner Magahud <abner@virtual-wonders.com>
2015-11-08 19:42:19 +0800

    added assets files and folders

---
+++ assets/ewealth/index.php
@@ -0,0 +1,113 @@
+<html>
+
+<head><title>EZwealthpages Online Opportunity</title>
+
+<script src="src/191.js"></script>
+<script src="src/ui.js"></script>
+<link rel="stylesheet" href="src/ui.css" />
+<link rel="stylesheet" href="index.css" />
+<link rel="shortcut icon" href="images/favicon.ico" />
+<script>
+$(document).ready(function(){            
+	$("#loginform").submit( function () { 
+	$("#error").html('<center><img src="icons/loader.gif"/></center>').show();
+		$.post('loginexec.html', $(this).serialize(), function(data){
+			$("#error").html(data)
+		});
+		return false;   
+	});
+});   
+</script>
+
+</head>
+<body>
+<div class="canvass">
+	<div class="header">
+		<div class="logo"></div>
+		<div style="float:right;">
+		<div id="error"><span style="clear:both;">| HOME | PRODUCTS | F.A.Q. | CONTACT US |</span></div>
+			<form id="loginform">
+				<table style="font-size:12px;color:#ffffff;font-family:arial;">
+				<tr><td>Username</td><td>Password</td><td></td></tr>
+				<tr>
+					<td><input style="width:100px;" type="text" name="username" value="" required></td>
+					<td><input style="width:100px;" type="password" name="pass" required></td>
+					<td><input type="submit" value="LOGIN"></td>
+				</tr>
+				</table>	
+			</form>
+		<div style="clear:both;"></div>	
+		</div>
+	</div>
+	<div style="clear:both;"></div>
+	
+	<div class="middle">	
+		<div class="vidleft">
+			<div style="padding:5px;">
+				
+				<iframe allowFullScreen allowTransparency="true" class="vzaar-video-player" frameborder="0" height="290" id="vzvd-2405996" mozallowfullscreen name="vzvd-2405996" src="http://view.vzaar.com/2405996/player" title="vzaar video player" type="text/html" webkitAllowFullScreen width="640"></iframe>
+				<div style="float:left;position:absolute;height:175px;width:200px;background-image:url(images/sideplayer.png);margin-top:-290px;"></div>
+			</div>
+		</div>
+		<div class="navright">
+			<div class="hblue">Join our Amazing Affiliate Program!</div>
+			<div style="padding:5px;">
+			<iframe allowFullScreen allowTransparency="true" class="vzaar-video-player" frameborder="0" height="255" id="vzvd-2310418" mozallowfullscreen name="vzvd-2310418" src="http://view.vzaar.com/2310418/player" title="vzaar video player" type="text/html" webkitAllowFullScreen width="328"></iframe>
+			</div>
+		</div>
+		
+		<div style="clear:both;"></div>	
+	</div>
+  <div class="middle">
+		<div class="one">
+			<div class="hblue">About Us</div>
+				<div style="font-family:arial;text-align:center;font-size:13px;padding:20px;">
+				<div style="font-weight:bold;">Who is EZwealthpages</div>
+				<div>EZ Wealth Builder is one of the fastest growing home-based business on the internet right now. The reason for our success is that we're operating right in the middle of a multi-billion dollar industry in the world which is personal development and global internet marketing... plus we have a breakthough product and system that easily works for anyone.</div>
+				</div>
+		</div>
+		<div class="two">
+			
+			<div class="hblue">EZwealthpages on Social Media</div>
+			<div style="width:225px;margin:0 auto;margin-top:15px;">
+				<a href="https://www.facebook.com/" target="_BLANK" style="text-decoration:none;">
+					<div style="float:left;width:75px;height:75px;background:url(images/facebook-icon.png);"></div>
+				</a>
+				<div style="float:left;width:75px;height:75px;background:url(images/twitter-icon.png);"></div>
+				<a href="https://www.youtube.com" target="_BLANK" style="text-decoration:none;">
+					<div style="float:left;width:75px;height:75px;background:url(images/youtube-icon.png);"></div>
+				</a>	
+			<div style="clear:both;"></div>
+		  </div>
+		</div>
+	<div class="three">
+			<div class="hblue">Why use EZwealthpages system?</div>
+				<div style="font-family:arial;text-align:justify;font-size:13px;padding:20px;">
+				  <p>* Not affiliated with any Company!<br>
+				    * No Programming skills needed!
+				    <br>
+				    * Simple Click and Type System!<br>
+				    * No Monthly Fees! No Hosting Fees!<br>
+				    * Highly Converting Capture Pages!<br>
+				    * 
+				    Use Multiple Capture Pages!<br>
+			    * You can add your own videos!</p></div>
+	  </div>		
+		<div style="clear:both;"></div>
+  </div>
+	
+	<div style="text-align:center;font-family:arial;font-size:12px;color:#ccc;margin-top:20px;">AVAILABLE CAPTURE PAGES<br>
+    Product names, logos, brands, and other trademarks featured or referred to within www.ezwealthpages.com products and services and within www.ezwealthpages.com are the property of their respective trademark holders. These trademark holders are not affiliated with www.ezwealthpages.com, our products, or our website. </div>
+	
+	<div class="footer">	
+	</div>
+</div>
+
+<script type="text/javascript">
+var _tcq = _tcq || []; _tcq.push(['sidebar', '7ada97cbee', 'right-top', '003060', 'ffffff']); 
+(function() { var e = document.createElement('script'); e.type = 'text/javascript'; e.async = true; e.src = '../s.tcimg.com/w/v2/sidebar.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(e, s); })();
+</script>
+
+</body>
+
+</html>
